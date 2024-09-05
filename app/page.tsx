247e2ef9795181ce3d87820d917a8e6c1701e184
James S <92613430+J-Saccutelli@users.noreply.github.com>
2024-09-05 11:54:43 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello World!</p>
+    <p>Hello World! This will be our new home for the app we build </p>
   );
 }
