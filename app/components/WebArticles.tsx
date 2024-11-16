1f2bd2a7a734396a5f84b6c844664fe1ed095061
James S <92613430+J-Saccutelli@users.noreply.github.com>
2024-11-15 19:18:36 -0500

    Update WebArticles.tsx

--- app/components/WebArticles.tsx
+++ app/components/WebArticles.tsx
@@ -1,1 +1,18 @@
+'use client';
 
+
+export type WebArticle = {
+  id?: number;
+  created_at?: string;
+  title: string;
+
+}
+
+
+export default function WebArticles(webArticle: { title }): {
+
+  webArticle:WebArticle
+} {
+  
+  return <div className="border px-4 py-3 cursor-pointer hover:bg-gray-900">{title}</div>;
+}
