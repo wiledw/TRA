4583afda7918b016f4205a3b1cc86b5d6f1fdeed
NicolasAvramidis <nicolas.avramidis@torontomu.ca>
2024-11-25 22:37:10 -0500

    add unban user api

---
+++ app/api/admin/unbanUser/route.ts
@@ -0,0 +1,33 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
+    }
+
+    // unban user
+    const { data, error } = await supabase
+        .from('user')
+        .update({ banned: false })
+        .eq('id', userId);
+
+    if (error) {
+      console.error('Error unbanning user:', error);
+      return NextResponse.json({ error: 'Error unbanning user' }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    console.error('Unexpected error:', err);
+    return NextResponse.json({ error: 'Unexpected error occurred' }, { status: 500 });
+  }
+}
